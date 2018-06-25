34aa9b3cf6c5ce06d44f629ea89eb9aaaebb454a
jiangjiesheng <dev@jiangjiesheng.com>
2018-06-25 12:37:22 +0800

    强制调教

---
+++ src/main/java/com/jiangjiesheng/imageloader/config/ImageResource.java
@@ -0,0 +1,15 @@
+
+package com.jiangjiesheng.imageloader.config; 
+/** 
+ * 类说明 ：
+ * @author 江节胜 E-mail:dev@jiangjiesheng.com 
+ * @version 创建时间：2017年4月2日 下午11:41:31 
+ */
+public class ImageResource {
+	public static int CurrentImageResource = -1;
+	public static int FromWeb = 0;
+	public static int FromSDCard = 1;
+	public static int FromContentProvider = 2;
+	public static int FromAssets = 3;
+	public static int FromDrawable = 4;
+}
